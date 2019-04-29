4e4219562720b3fdde4d2c3a472ba945149e1949
mattorus <mattorus@gmail.com>
2019-04-29 12:05:53 -0700

    Complete refactor

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,50 +4,31 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    [SerializeField] int colorInteger = ColorNG.maxColor;
-    [SerializeField] Color32 color;
-    [SerializeField] byte r = 100;
-    [SerializeField] byte g = 100;
-    [SerializeField] byte b = 100;
-    [SerializeField] string colorHex = "0x646464";
+    [SerializeField] int colorInteger;
+    [SerializeField] string colorHex;
 
     SpriteRenderer spriteRenderer;
 
     private void UpdateColor()
     {
         colorHex = System.Convert.ToString(colorInteger, toBase: 16);
-        SetColor(ColorNG.CreateColor(r, g, b));
+        SetColor(ColorNG.CreateColor(colorInteger));
     }
 
     private void RandomizeColor()
     {
         colorInteger = ColorNG.GenerateRandomColor();
-        // Move below code into ColorNG class
-        b = (byte)(colorInteger % 256);
-        g = (byte)((colorInteger / 256) % 256);
-        r = (byte)((colorInteger / 65535) % 256);
     }
-
-    //private UnityEngine.Color CreateColor(byte r, byte g, byte b)
-    //{
-    //    r = (byte)(r * (r + 40) / 2);
-    //    g = (byte)((g + 10) / 2);
-    //    b = (byte)((b + 200) / 2);
-
-    //    var retVar = new UnityEngine.Color(r / 255f, g / 255f, b / 255f);
-    //    return retVar; 
-    //}
 
     private void SetColor(UnityEngine.Color color)
     {
         spriteRenderer.color = color;
-        //spriteRenderer = GetComponent<SpriteRenderer>();
-        //spriteRenderer.color = color;
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        colorInteger = ColorNG.maxColor;
         spriteRenderer = GetComponent<SpriteRenderer>();
         UpdateColor();
     }
--- Assets/Scripts/ColorNG.cs
+++ Assets/Scripts/ColorNG.cs
@@ -13,14 +13,40 @@ public class ColorNG : MonoBehaviour
         return retVal;
     }
 
+    public static UnityEngine.Color CreateColor(int colorInteger)
+    {
+        colorInteger = (colorInteger > maxColor ? maxColor : colorInteger);
+
+        var r = (byte)((colorInteger / 65536) % 256);
+        var g = (byte)((colorInteger / 256) % 256);
+        var b = (byte)(colorInteger % 256);
+
+        var retVal = CreateColor(r, g, b);
+        return retVal;
+    }
+    
     public static UnityEngine.Color CreateColor(byte r, byte g, byte b)
     {
-        // The below code is for mixing colors before creation
-        //r = (byte)(r * (r + 40) / 2);
-        //g = (byte)((g + 10) / 2);
-        //b = (byte)((b + 200) / 2);
-
-        var retVar = new UnityEngine.Color(r / 255f, g / 255f, b / 255f);
+        var retVar = new Color(r / 255f, g / 255f, b / 255f);
         return retVar;
     }
+
+    public static UnityEngine.Color MixColor(UnityEngine.Color color, byte rMix, byte gMix, byte bMix)
+    {
+        color.r = (byte)(color.r * (color.r + rMix) / 2);
+        color.g = (byte)((color.g + gMix) / 2);
+        color.b = (byte)((color.b + bMix) / 2);
+
+        return color;
+    }
+
+    public static UnityEngine.Color MixColor(UnityEngine.Color color, UnityEngine.Color colorMix)
+    {
+        color.r = (byte)(color.r * (color.r + colorMix.r) / 2);
+        color.g = (byte)((color.g + colorMix.g) / 2);
+        color.b = (byte)((color.b + colorMix.b) / 2);
+
+        return color;
+    }
+
 }
